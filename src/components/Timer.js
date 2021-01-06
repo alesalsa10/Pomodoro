666891002b9d193e273a8c8f76db5e19e56d923b
alesalsa10 <alexandersalsa10@gmail.com>
2021-01-05 20:25:48 -0500

    added font picker

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -11,12 +11,16 @@ export default function Timer() {
   const [shortBreakSetting, setShortBreakSetting] = useState(shortBreak);
   const [longBreakSetting, setLongBreakSetting] = useState(longBreak);
 
-  const [minutes, setMinutes] = useState('25');
+  const [minutes, setMinutes] = useState();
   const [seconds, setSeconds] = useState('00');
+
+  const [font, setFont] = useState('roboto');
+  const [fontSetting, setFontSetting] = useState(font);
+
   const [startCountdown, setStartCountdown] = useState(false);
   const [action, setAction] = useState('START');
   const [selected, setSelected] = useState('pomodoro');
-  const [indicator, setIndicator] = useState('');
+  const [indicator, setIndicator] = useState();
   const [totalTime, setTotalTime] = useState();
   const [modalIsOpen, setIsOpen] = useState(false);
 
@@ -31,7 +35,7 @@ export default function Timer() {
       marginRight: '-50%',
       transform: 'translate(-50%, -50%)',
       width: '400px',
-      borderRadius: '15px'
+      borderRadius: '15px',
     },
     overlay: {
       backgroundColor: 'rgba(0,0,0,0)',
@@ -41,11 +45,16 @@ export default function Timer() {
   Modal.setAppElement('body');
 
   function openModal() {
+    setStartCountdown(false);
+    setAction('START');
     setIsOpen(true);
   }
 
   function closeModal() {
+    setStartCountdown(false);
+    setAction('START');
     setIsOpen(false);
+    setIndicator(Math.floor(Math.random() * 1000));
   }
 
   function updateTime() {
@@ -55,7 +64,7 @@ export default function Timer() {
       setAction('DONE');
     } else {
       if (seconds == 0) {
-        if (minutes > 0 && minutes <= 10) {
+        if (parseInt(minutes) > 0 && parseInt(minutes) < 10) {
           setSeconds(59);
           setMinutes(`0${minutes - 1}`);
         } else {
@@ -82,9 +91,9 @@ export default function Timer() {
 
   const handleTimeClicks = (e) => {
     setStartCountdown(false);
-    setAction('START');
     setIndicator(Math.floor(Math.random() * 1000));
     setSeconds('00');
+    setAction('START');
     if (e.target.id === 'pomodoro') {
       setMinutes(pomodoroTime);
       setSelected('pomodoro');
@@ -97,64 +106,87 @@ export default function Timer() {
     }
   };
 
-  const handleNumberChange = e => {
-    if(e.target.id === 'pomodoro' ){
-      if (e.target.value < 10){
-        setPomodoroSetting(`0${e.target.value}`)
+  const handleNumberChange = (e) => {
+    if (e.target.id === 'pomodoro') {
+      if (e.target.value < 10) {
+        setPomodoroSetting(`0${e.target.value}`);
       } else {
-        setPomodoroSetting(e.target.value)
+        setPomodoroSetting(e.target.value);
       }
-    } else if (e.target.id === 'shortBreak'){
-      if (e.target.value < 10){
-        setShortBreakSetting(`0${e.target.value}`)
+    } else if (e.target.id === 'shortBreak') {
+      if (e.target.value < 10) {
+        setShortBreakSetting(`0${e.target.value}`);
       } else {
-        setShortBreakSetting(e.target.value)
+        setShortBreakSetting(e.target.value);
       }
     } else {
-      if (e.target.value < 10){
-        setLongBreakSetting(`0${e.target.value}`)
+      if (e.target.value < 10) {
+        setLongBreakSetting(`0${e.target.value}`);
       } else {
-        setLongBreakSetting(e.target.value)
+        setLongBreakSetting(e.target.value);
       }
     }
-  }
+  };
 
-  const handleApplySettings = e => {
-    setPomodoroTime(pomodoroSetting)
-    setShortBreak(shortBreakSetting)
-    setLongBreak(longBreakSetting)
-    closeModal()
-  }
+  const handleFontClick = (e) => {
+    if (e.target.id === 'roboto') {
+      setFontSetting('roboto');
+    } else if (e.target.id === 'lato') {
+      setFontSetting('lato');
+    } else {
+      setFontSetting('xanhMono');
+    }
+  };
+
+  const handleApplySettings = (e) => {
+    setSeconds('00');
+    setPomodoroTime(pomodoroSetting);
+    setShortBreak(shortBreakSetting);
+    setLongBreak(longBreakSetting);
+    setFont(fontSetting);
+    closeModal();
+  };
 
   useEffect(() => {
     setTotalTime(parseInt(minutes) * 60 + parseInt(seconds));
   }, [selected, indicator]);
 
-  
   useEffect(() => {
     const interval = setTimeout(function () {
       if (!startCountdown) {
         //do nothing
+        
       } else {
         updateTime();
       }
     }, 1000);
-    return ()=> interval
+    return () => interval;
   }, [startCountdown, seconds, minutes]);
 
   useEffect(() => {
-    if (selected === 'pomodoro'){
-      setMinutes(pomodoroTime)
+    if (selected === 'pomodoro') {
+      setMinutes(pomodoroTime);
     } else if (selected === 'shortBreak') {
-      setMinutes(shortBreak)
+      setMinutes(shortBreak);
     } else {
-      setMinutes(longBreak)
+      setMinutes(longBreak);
     }
-  })
+  }, [
+    pomodoroSetting,
+    shortBreakSetting,
+    longBreakSetting,
+    pomodoroTime,
+    shortBreak,
+    longBreak,
+  ]);
 
   return (
-    <div>
-      <div className='timer-card'>
+    <div
+      className={`${
+        font === 'roboto' ? 'roboto' : font === 'lato' ? 'lato' : 'xanhMono'
+      }`}
+    >
+      <div className={`${'timerCard'}`}>
         <div className='buttons-div'>
           <div
             className={`${'buttons'} ${
@@ -229,7 +261,6 @@ export default function Timer() {
             fontSize='2px'
           >
             {minutes}:{seconds}
-            
           </text>
           <text
             x='5'
@@ -261,35 +292,85 @@ export default function Timer() {
           style={customStyles}
           contentLabel='Settings Modal'
         >
-          <h2>Settings</h2>
-          <h4>TIME (MINUTES)</h4>
-          <div className='configureTimes'>
-            <div className='pomodoro'>
-              <div className="header">pomodoro</div>
-              <input type="number" name="pomodoro" id="pomodoro" defaultValue={parseInt(pomodoroTime)} max='60' min='5' onChange={handleNumberChange} />
+          <div
+            className={`${
+              font === 'roboto'
+                ? 'roboto'
+                : font === 'lato'
+                ? 'lato'
+                : 'xanhMono'
+            }`}
+          >
+            <h2>Settings</h2>
+            <h4>TIME (MINUTES)</h4>
+            <div className='configureTimes'>
+              <div className='pomodoro'>
+                <div className='header'>pomodoro</div>
+                <input
+                  type='number'
+                  name='pomodoro'
+                  id='pomodoro'
+                  defaultValue={parseInt(pomodoroTime)}
+                  max='60'
+                  min='5'
+                  onChange={handleNumberChange}
+                />
+              </div>
+              <div className='shortBreak'>
+                <div className='header'>short break</div>
+                <input
+                  type='number'
+                  name='shortBreak'
+                  id='shortBreak'
+                  defaultValue={parseInt(shortBreak)}
+                  max='15'
+                  min='1'
+                  onChange={handleNumberChange}
+                />
+              </div>
+              <div className='longBreak'>
+                <div className='header'>long break</div>
+                <input
+                  type='number'
+                  name='longBreak'
+                  id='longBreak'
+                  defaultValue={parseInt(longBreak)}
+                  max='25'
+                  min='5'
+                  onChange={handleNumberChange}
+                />
+              </div>
             </div>
-            <div className='shortBreak'>
-              <div className="header">short break</div>
-              <input type="number" name="shortBreak" id="shortBreak" defaultValue={parseInt(shortBreak)} max='15' min='1' onChange={handleNumberChange} />
+            <div className='configureFont'>
+              <h4 className='header fontColor '>FONT</h4>
+              <div
+                className='roboto fonts'
+                id='roboto'
+                onClick={handleFontClick}
+              >
+                Aa
+              </div>
+              <div
+                className='xanhMono fonts'
+                id='xanhMono'
+                onClick={handleFontClick}
+              >
+                Aa
+              </div>
+              <div className='lato fonts' id='lato' onClick={handleFontClick}>
+                Aa
+              </div>
             </div>
-            <div className='longBreak'>
-              <div className="header">long break</div>
-              <input type="number" name="longBreak" id="longBreak" defaultValue={parseInt(longBreak)} max='25' min='5' onChange={handleNumberChange} />
+            <div className='configureColor'>
+              <h4 className='header fontColor '>COLOR</h4>
+              <div className='color colorRed '>Aa</div>
+              <div className='color colorBlue '>Aa</div>
+              <div className='color colorPurple '>Aa</div>
+            </div>
+            <div className='applyButton' onClick={handleApplySettings}>
+              Apply
             </div>
           </div>
-          <div className="configureFont">
-            <h4 className="header">FONT</h4>
-            <div className="font1">Aa</div>
-            <div className="font2">Aa</div>
-            <div className="font3">Aa</div>
-          </div>
-          <div className="configureColor">
-            <h4 className="header">COLOR</h4>
-            <div className="color colorRed ">Aa</div>
-            <div className="color colorBlue ">Aa</div>
-            <div className="color colorPurple ">Aa</div>
-          </div>
-          <div className="applyButton" onClick={handleApplySettings} >Apply</div>
         </Modal>
       </div>
     </div>
